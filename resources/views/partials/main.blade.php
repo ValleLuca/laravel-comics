65406688fe8888291d8734a9e8e1b6eeae7d8dd4
ValleLuca <valle.lucaAb@outlook.it>
2022-03-03 18:12:41 +0100

    Aggiunti fumetti

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,3 +1,17 @@
 <div class="container_main">
     <div class="jumbotron"></div>
+
+    <div class="d-flex flex-wrap justify-content-center">
+        @foreach ($fumetti as $index => $elements)
+            <div class="dBox">
+                <div class="margine">
+                    <img src="{{$elements['thumb']}}">
+                    <h4>{{$elements['title']}}</h4>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    
+   
+
 </div>
