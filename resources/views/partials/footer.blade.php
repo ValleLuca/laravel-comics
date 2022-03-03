8b955c04f86902da865c025614c12fbf90de0abc
ValleLuca <valle.lucaAb@outlook.it>
2022-03-03 18:24:15 +0100

    Aggiunto parte finale footer/ da concludere

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,9 +1,20 @@
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <h2>footer</h2>
+<div class="container_footer">
+    <div class="lastFooter">
+        <div class="container containerFooter">
+          <div>
+            <div class="sign">SIGN-UP NOW!</div>
+          </div>
+          <div class="dFlexIcons">
+            <h3>FOLLOW US</h3>
+            <div class="pIcons">
+              <img src="{{asset('img/footer-facebook.png')}}">
+              <img src="{{asset('img/footer-periscope.png')}}">
+              <img src="{{asset('img/footer-pinterest.png')}}">
+              <img src="{{asset('img/footer-twitter.png')}}">
+              <img src="{{asset('img/footer-youtube.png')}}">
+            </div>
+          </div>
         </div>
-    </div>
+    </div>    
 </div>
-
 @yield('utili')
