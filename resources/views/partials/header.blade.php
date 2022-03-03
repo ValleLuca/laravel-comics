ea00d6cae9d977b1018592ef3b11ab89f5ece311
ValleLuca <valle.lucaAb@outlook.it>
2022-03-03 16:22:03 +0100

    Inizio

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row text-center">
+        <div class="col">
+            <h2>header</h2>
+            <div>
+                @include('partials.sectionav.nav')
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/partials/sectionav/nav.blade.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <ul class="d-flex flex-row">
+                <li class="p-1"><a href="#">1</a></li>
+                <li class="p-1"><a href="#">2</a></li>
+                <li class="p-1"><a href="#">3</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
